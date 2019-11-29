5e68c92340c59ff7db810a29ab2cdb5c484d2cde
Brian Richings <32361300+Jewishwookie@users.noreply.github.com>
2019-11-29 11:06:21 +0000

    more script work

--- Penguin Island/Assets/Scripts/Fish.cs
+++ Penguin Island/Assets/Scripts/Fish.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Fish : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float fSpeed;
+
+    private float randomisedSpeed = 0f;
+    private float nextActionTime = -1f;
+    private Vector3 targetPosition;
+
+    private void FixedUpdate()
+    {
+        if (fSpeed > 0)
+        {
+            Swim();
+        }
+    }
+
+    private void Swim()
+    {
+        if(Time.fixedTime >= nextActionTime)
+        {
+            //Randomise speed
+            randomisedSpeed = fSpeed * UnityEngine.Random.Range(.5f, 1.5f);
+
+            //pick random target
+            targetPosition = PenguinArea.chooseRandPos(transform.parent.position, 100f, 260f, 2f, 13f);
+
+            //rotate toward the target
+            transform.rotation = Quaternion.LookRotation(targetPosition - transform.position, Vector3.up);
+        }
+    }
 }
--- Penguin Island/Assets/Scripts/PenguinAgent.cs
+++ Penguin Island/Assets/Scripts/PenguinAgent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MLAgents;
+using System;
 
 public class PenguinAgent : Agent {
 
@@ -30,8 +31,94 @@ public class PenguinAgent : Agent {
         }
 
         //animator parameters
-
         animator.SetFloat("Vertical", forward);
         animator.SetFloat("Horizontal", leftOrRight);
+
+        //reward every step
+        AddReward(-1f / agentParameters.maxStep);
+    }
+
+    public override void AgentReset()
+    {
+        fullTummy = false;
+        pArea.ResetArea();
+    }
+    public override void CollectObservations()
+    {
+        //Penguin eaten?
+        AddVectorObs(fullTummy);
+
+        //distance to baby
+        AddVectorObs(Vector3.Distance(baby.transform.position, transform.position));
+        //direction to baby
+        AddVectorObs((baby.transform.position - transform.position).normalized);
+
+        //direction penguin is facing
+        AddVectorObs(transform.forward);
+
+        //RayPerception (sight)
+        float rayDistance = 20f;
+        float[] rayAngles = { 30f, 60f, 90f, 120f, 150f };
+        string[] detectableObjects = { "baby", "fish", "wall" };
+
+        AddVectorObs(rayPerception.Perceive(rayDistance, rayAngles, detectableObjects,0f, 0f));
+    }
+
+    private void Start()
+    {
+        pArea = GetComponentInParent<PenguinArea>();
+        baby = pArea.pBaby;
+        animator = GetComponent<Animator>();
+        rayPerception = GetComponent<RayPerception3D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position,baby.transform.position) < pArea.fRadius)
+        {
+            //close enough to try to feed
+            RegurgitateFish();
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("fish"))
+        {
+            EatFish(collision.gameObject);
+        }
+        else if (collision.transform.CompareTag("baby"))
+        {
+            RegurgitateFish();
+        }
+    }
+
+    private void EatFish(GameObject fishObject)
+    {
+        if (fullTummy) return;
+        fullTummy = true;
+
+        pArea.removeSpecificFish(fishObject);
+        AddReward(1f);
+    }
+
+    private void RegurgitateFish()
+    {
+        if (!fullTummy) return;
+        fullTummy = false;
+
+        //generate regurgitated fish
+        GameObject regurgitatedFish = Instantiate<GameObject>(regurgFishPrefab);
+        regurgitatedFish.transform.parent = transform.parent;
+        regurgitatedFish.transform.position = baby.transform.position;
+        Destroy(regurgitatedFish, 4f);
+
+        //spawn heart
+        GameObject heart = Instantiate<GameObject>(heartPrefab);
+        heart.transform.parent = transform.parent;
+        heart.transform.position = baby.transform.position + Vector3.up;
+        Destroy(heart, 4f);
+
+        AddReward(1f);
     }
 }
